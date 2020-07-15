a8d60109a1aa10e5acc5211c8af02fa7df598042
Daniyar Shulakov <dshulakov@gmail.com>
2020-07-15 18:20:10 +0600

    first commit

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.use(cors());
 app.get("/", function(request, response){
     response.send("Hello from server! now " + Date.now());
 });
-console.log("Server start on port 3000");
+console.log("Hello! Server start on port 3000");
 app.listen(3000);
