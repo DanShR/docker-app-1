8b3cbe3eb211066f4afcb35f49f47d0a93ad6276
Daniyar Shulakov <dshulakov@gmail.com>
2020-07-16 10:57:47 +0600

    index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var cors = require('cors')
 app.use(cors());
 app.get("/", function(request, response){
-    response.send("Hello from server! now " + Date.now());
+    response.send("Hello, my friend, from server! now " + Date.now());
 });
 console.log("Hello! Server start on port 3000");
 app.listen(3000);
